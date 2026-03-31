d9aca46c257ea8c52b84c952f8a5d46dd57d763d
Nathan Schoenike <natschoenike711@gmail.com>
2026-03-31 02:34:31 +0000

    Set up files

---
+++ playerteam.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+class PlayerTeam()
+{
+    protected:
+    Member member1
+    Member member2
+
+
+    public:
+
+            PlayerTeam PlayerTeam()
+            {
+
+            }
+}
